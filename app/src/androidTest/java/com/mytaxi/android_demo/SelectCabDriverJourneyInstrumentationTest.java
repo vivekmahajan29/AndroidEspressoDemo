6fb4cd94c272c27e3e68ba3741c0468629654f34
Vivek Mahajan <Vivek_Mahajan@external.mckinsey.com>
2018-10-24 22:02:52 +0530

    Changing the name of the Instrumentation Test

--- app/src/androidTest/java/com/mytaxi/android_demo/SelectCabDriverJourneyInstrumentationTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/SelectCabDriverJourneyInstrumentationTest.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class SelectCabDriverJourneyInstrumentationTest {
 
     @Rule
     public ActivityTestRule mActivityRule = new ActivityTestRule<>(MainActivity.class);
